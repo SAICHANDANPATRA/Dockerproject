a74bc5e05f72ca6ac0f9afa684cf533a90297454
SAICHANDANPATRA <2170203@kiit.ac.in>
2023-07-28 23:40:36 +0530

    dockorizing project

---
+++ src/test/java/org/jt/dockerimg/DockerimgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jt.dockerimg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerimgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
